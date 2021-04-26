7e3b29f832fe0c58aa717439a95631dcf2953736
Affan Ilyas <affan.ilyas@hpam.co.id>
2021-04-26 13:48:34 +0700

    commit pin page

--- src/componens/PinPage.js
+++ src/componens/PinPage.js
@@ -87,7 +87,7 @@ class PinPage extends React.Component {
           return response.json().then(obj => {
             res = obj;
             Alert.alert('Sukses', 'Transaksi berhasil. Segera lakukan pembayaran sebelum masa berlaku berakhir',
-              [{ text: 'OK', onPress: () => this.props.navigation.navigate('Activity',{update:'1'}) }],
+              [{ text: 'OK', onPress: () => this.props.navigation.navigate('Order',{update:'1'}) }],
               { cancelable: false },
             );
           })
@@ -371,6 +371,7 @@ class PinPage extends React.Component {
 
   pressGoBack(){
     const { params } = this.props.navigation.state;
+    this.props.navigation.navigate('Home')
     if(params.title == 'SUB'){
       this.props.navigation.navigate('Buy', { id: params.id, nilaiInvest: params.value })
     }else if(params.title == 'RED'){
